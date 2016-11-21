cf32e92650fa83bdbc1f034306a2b71757008965
Unai Alegre <u.alegre@mdx.ac.uk>
2016-11-21 18:16:34 +0000

    Renamed the project to lower-case

--- cmd2acl/src/cmd2acl/Activator.java
+++ cmd2acl/src/cmd2acl/Activator.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-package CMD2ACL;
+package cmd2acl;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
@@ -21,7 +21,7 @@ public class Activator extends Plugin {
     /**
      * The plug-in ID.
      */
-    public static final String PLUGIN_ID = "CMD2ACL";
+    public static final String PLUGIN_ID = "cmd2acl";
 
     /**
      * The shared instance.
--- cmd2acl/src/cmd2acl/main/Main.java
+++ cmd2acl/src/cmd2acl/main/Main.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-package CMD2ACL.main;
+package cmd2acl.main;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,7 +35,7 @@ public class Main extends AbstractAcceleoGenerator {
      *
      * @generated
      */
-    public static final String MODULE_FILE_NAME = "/CMD2ACL/main/main";
+    public static final String MODULE_FILE_NAME = "/cmd2acl/main/main";
     
     /**
      * The name of the templates that are to be generated.
